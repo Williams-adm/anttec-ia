fc0bd87577510b436b65f3cd17fd3d89112dac59
WilliamsAdm <i2310607@continental.edu.pe>
2026-02-09 20:23:00 -0500

    volviendo al inicio

--- main.py
+++ main.py
@@ -125,9 +125,4 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    import os
-    
-    # Railway asigna el puerto dinámicamente
-    port = int(os.getenv("PORT", 8001))
-    
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
